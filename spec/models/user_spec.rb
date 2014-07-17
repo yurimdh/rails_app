f38b75c5ead218f76ad7cf6238ba33cd5e7f1b31
Yuri Santos <yurisantos47@gmail.com>
2014-07-17 13:41:14 -0300

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,10 +12,16 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+  
   describe "when name is not present" do
     before { @user.name = " " }
 
@@ -108,5 +114,6 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+
   end
 end
